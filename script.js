9672c81eaecf9b6a3099ca368f6d187268109e0f
Corentin <corentin@gmail.com>
2024-06-10 09:50:18 +0200

    setting up

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let canv = document.querySelector("canvas");
+let ctx = canv.getContext("2d");
+
+canv.width = window.innerWidth;
+canv.height = window.innerHeight;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0,0,canv.width, canv.height);
